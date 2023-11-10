70005a83c8c333668dedc53cca74810282e4246a
Adamxu3d <adamxu3d@yahoo.com>
2023-11-09 22:57:45 -0600

    Final commit

--- src/components/NftTransfers.js
+++ src/components/NftTransfers.js
@@ -1,10 +1,38 @@
-import { Button, Modal } from "antd";
+import { Button, Modal, Skeleton, Table, Tooltip } from "antd";
 import { useEffect, useState } from "react";
-import { infoCircleOutlined } from "@ant-design/icons";
+import { InfoCircleOutlined } from "@ant-design/icons";
 import { getNFTTransfers } from "../utils";
+
+const columns = [
+  {
+    title: "Transfer DateTime",
+    dataIndex: "block_timestamp",
+    key: "block_timestamp",
+    render: (value) => {
+      return new Date(value).toLocaleString();
+    },
+  },
+  {
+    title: "Amount",
+    dataIndex: "amount",
+    key: "amount",
+  },
+  {
+    title: "From Address",
+    dataIndex: "from_address",
+    key: "from_address",
+  },
+  {
+    title: "To Address",
+    dataIndex: "to_address",
+    key: "to_address",
+  },
+];
+
 const ModalContent = ({ nft }) => {
   const [loading, setLoading] = useState(true);
   const [data, setData] = useState([]);
+
   const { token_address, token_id } = nft;
 
   useEffect(() => {
@@ -15,34 +43,40 @@ const ModalContent = ({ nft }) => {
       .finally(() => {
         setLoading(false);
       });
-  });
+  }, []);
+
+  if (loading) {
+    return <Skeleton active />;
+  }
+
+  return (
+    <Table columns={columns} dataSource={data} pagination={{ pageSize: 5 }} />
+  );
 };
-// 解构语法
-// defines the name of the NFT
+
 const NftTransfers = ({ nft }) => {
   const [modalOpen, setModalOpen] = useState(false);
+
   return (
     <>
-      <tooltip title="Transfer(s) on this NFT">
+      <Tooltip title="Transfer(s) on this NFT">
         <Button
-          type="primary"
           style={{ border: "none" }}
           size="large"
           shape="circle"
-          icon={<infoCircleOutlined />}
+          icon={<InfoCircleOutlined />}
           onClick={() => setModalOpen(true)}
         />
-      </tooltip>
-
+      </Tooltip>
       <Modal
         width={1000}
-        title="Transfer(s) list"
+        title="Transfer(s) List"
         destroyOnClose
         open={modalOpen}
         footer={null}
         onCancel={() => setModalOpen(false)}
       >
-        Nft Transfers
+        <ModalContent nft={nft} />
       </Modal>
     </>
   );
